fab5298b075c3d7a8f76ba59ecd91585a65cd77a
Billy <billybofh@gmail.com>
2019-04-18 10:23:34 +0100

    Refactoring to use a common base dockerfile

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 
 BASE_NAME="uogsoe/soe-php-apache"
-VERSIONS=( "7.1" "7.2" "7.3" "7.1-ci" "7.2-ci" "7.3-ci" "latest")
+#VERSIONS=( "7.1" "7.2" "7.3" "7.1-ci" "7.2-ci" "7.3-ci" "latest")
+VERSIONS=( "7.1" "7.2" "7.3" )
 
 PNAME=`basename $0`
 LOGFILE=`mktemp /tmp/${PNAME}.XXXXXX` || exit 1
@@ -11,14 +12,14 @@ echo "Logging to ${LOGFILE}"
 
 for VERSION in "${VERSIONS[@]}";
 do
-    FILENAME=Dockerfile.${VERSION}
-    if [ ! -f $FILENAME ];
-    then
-        echo "No such file $FILENAME"
-        exit 1
-    fi
     echo "Building ${VERSION}..."
-    docker build -t "${BASE_NAME}":"${VERSION}" -f "${FILENAME}" . >> "${LOGFILE}"
+    docker build --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}" -f Dockerfile.base . >> "${LOGFILE}"
     echo "Pushing ${VERSION}..."
-    docker push "${BASE_NAME}":"${VERSION}" >> "${LOGFILE}"
+    #docker push "${BASE_NAME}":"${VERSION}" >> "${LOGFILE}"
+
+    echo "Building ${VERSION}-ci..."
+    docker build --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}-ci" -f Dockerfile.base . >> "${LOGFILE}"
+    echo "Pushing ${VERSION}-ci..."
+    #docker push "${BASE_NAME}":"${VERSION}-ci" >> "${LOGFILE}"
 done
+
